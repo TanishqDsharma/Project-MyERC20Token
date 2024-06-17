8337144debb4f3324c8ffeebffb8a1acf9f542ea
TanishqDsharma <tanishqsharma612@gmail.com>
2024-06-17 05:12:52 +0000

    Added Mint Functionality to mint tokens easily

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -2,4 +2,10 @@
 
 pragma solidity ^0.8.16;
 
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol" ;
 
+contract MyToken is ERC20{
+    constructor() ERC20("LOLCoin","LOL") {
+        _mint(msg.sender,1000e18);
+    }
+}
