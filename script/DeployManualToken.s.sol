d13eb0c3c35bcfea0604d746a1eae1894b7fc2a6
Tanishq <43058749+TanishqDsharma@users.noreply.github.com>
2024-06-22 13:44:59 +0530

    Update DeployManualToken.s.sol

--- script/DeployManualToken.s.sol
+++ script/DeployManualToken.s.sol
@@ -6,12 +6,17 @@ import {Script} from    "../lib/forge-std/src/Script.sol";
 import {ManualToken} from "../src/ManualToken.sol";
 
 contract DeployManualToken is Script {
-
-ManualToken manualToken;
+uint256 public DEFAULT_ANVIL_PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+uint256 public deployerKey;
 
 function run() external returns(ManualToken){
-    vm.startBroadcast();
-    manualToken = new ManualToken(10000);
+    if (block.chainid == 31337) {
+        deployerKey = DEFAULT_ANVIL_PRIVATE_KEY;
+    } else {
+        deployerKey = vm.envUint("PRIVATE_KEY");
+    }
+    vm.startBroadcast(deployerKey);
+    ManualToken manualToken = new ManualToken(100);
     vm.stopBroadcast();
     return manualToken;
 }
