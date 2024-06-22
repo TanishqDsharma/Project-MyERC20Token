099fd5be2605deffb5867b987615b0c7504e12b4
Tanishq <43058749+TanishqDsharma@users.noreply.github.com>
2024-06-22 13:48:50 +0530

    Update MyToken.sol
    Using openzeppilin for minting token

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.16;
 
+// Using OpenZeppelin for Minting Token
 import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol" ;
 
 contract MyToken is ERC20{
